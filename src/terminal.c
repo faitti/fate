526798541bb4ab0a836ca7977f4f61d6927c5e03
faitti <aleksanteri.nissinen@jyno.fi>
2023-02-15 14:49:34 +0200

    feat(flags): more flags so we get actual raw mode

--- src/terminal.c
+++ src/terminal.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 struct termios origin;
 
@@ -14,15 +16,25 @@ void fate_enable_raw_mode() {
     atexit(fate_disable_raw_mode);
 
     struct termios raw = origin;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 0;
 }
 
 void fate_raw_mode() {
     fate_enable_raw_mode();
 
     char input;
-    while (read(STDIN_FILENO, &input, 1) == 1) {
-        if (input == 'q') return;
+    while (read(STDIN_FILENO, &input, 1) == 1 && input != 'q') {
+       if (iscntrl(input)) {
+            printf("%d\n", input);
+       } else {
+            printf("%d ('%c')\n", input, input);
+       }
     }
 }
